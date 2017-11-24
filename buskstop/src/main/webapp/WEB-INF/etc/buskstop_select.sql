3a22e1e0b9725501375827dd4206208795f23f1a
LeeDongYeop <hjyj4841@naver.com>
2017-11-24 15:30:48 +0900

    끝?

--- buskstop/src/main/webapp/WEB-INF/etc/buskstop_select.sql
+++ buskstop/src/main/webapp/WEB-INF/etc/buskstop_select.sql
@@ -319,6 +319,23 @@ where v.video_artist like '%'||artist_name||'%';
 /* 내가(id1) 단 댓글 조회 */
 SELECT * FROM PERFORMANCE_COMMENT WHERE PERFORMANCE_COMMENT_USER_ID='id1';
 
+/* 좋아요한 공연영상 조회 */
+select v.*
+from VIDEO_LIKE vl, VIDEO v
+where vl.video_like_no = v.video_id
+and vl.video_like_user_id = 'hong1653'; /* 조회할 회원 id */
+
+/* 좋아요한 공연정보 조회 */
+select p.*
+from PERFORMANCE p, PERFORMANCE_LIKE pl
+where pl.performance_like_no = p.performance_no
+and pl.performance_like_user_id = 'kimm990'; /* 조회할 회원 id */
+
+/* 올린 고객센터 글 조회 */
+select *
+from HELP
+where help_user_id = 'kimbo88'; /* 조회할 회원 id */
+
 
 /**********************동영상(게시물)*****************************************/
 
