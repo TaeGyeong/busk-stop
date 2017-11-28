a2ebd33722df3c2a0a7185190e278bbe8e1d4663
UmTaegyeong <um006500@gmail.com>
2017-11-28 10:13:32 +0900

    d

--- buskstop/src/main/java/com/buskstop/controller/MyPageController.java
+++ buskstop/src/main/java/com/buskstop/controller/MyPageController.java
@@ -21,13 +21,18 @@ public class MyPageController {
 	@Autowired
 	private StageService stageService;
 	
+	/*@RequestMapping("/member/modify")
+	public ModelAndView memberModify() {
+		
+		return null;
+	}*/
+	
 	
 	@RequestMapping("/member/registArtist")
 	public String registArtist(String artistName, String performance, String profile,String artistImage) {
 		
 		// 사용자의 id 조회
-		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-		String id = ((User)authentication.getPrincipal()).getUserId();
+		String id = getUserId();
 		
 		// business logic
 		artistService.registerArtist(new Artist(id, artistName, performance, profile, artistImage, artistName));
@@ -38,9 +43,13 @@ public class MyPageController {
 	
 	@RequestMapping("/member/registSupplier")
 	public String registSupplier(String establishNum, String operatorNum, String stageName, String stageLocation, String stageArea, String stageImage) {
-		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-		String id = ((User)authentication.getPrincipal()).getUserId();
+		String id = getUserId();
 		stageService.registerSupplier(new StageSupplier(Integer.parseInt(establishNum),Integer.parseInt(operatorNum),stageName,stageLocation,Integer.parseInt(stageArea),stageImage,id));
 		return "user/registerSuccessView.tiles";
+	}
+	
+	// Security context 값을 받아서 userId 를 받는 method
+	private String getUserId() {
+		return ((User)SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getUserId();
 	}
 }
