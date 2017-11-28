85b2b9b55c8da872cbe55d25ae1dde6fa24c2b49
LeeDongYeop <hjyj4841@naver.com>
2017-11-28 19:06:22 +0900

    공연정보 조회(날짜조회 제외) 구현

--- buskstop/src/main/java/com/buskstop/service/PerformanceService.java
+++ buskstop/src/main/java/com/buskstop/service/PerformanceService.java
@@ -1,5 +1,6 @@
 package com.buskstop.service;
 
+import java.util.Date;
 import java.util.List;
 
 import com.buskstop.vo.Performance;
@@ -19,5 +20,41 @@ public interface PerformanceService {
 	void updatePerformance(Performance performance);
 	
 	List<Performance> selectAllPerformance();
+	/**
+	 * 공연정보 제목으로 검색
+	 * @param performanceTitle
+	 * @return
+	 */
+	List<Performance> selectPerformanceByPerformanceTitle(String performanceTitle);
+	/**
+	 * 공연정보 작성자로 검색
+	 * @param performanceUserId
+	 * @return
+	 */
+	List<Performance> selectPerformanceByPerformanceUserId(String performanceUserId);
+	/**
+	 * 공연정보 공연장소로 검색
+	 * @param performanceLocation
+	 * @return
+	 */
+	List<Performance> selectPerformanceByPerformanceLocation(String performanceLocation);
+	/**
+	 * 공연정보 공연이름으로 검색
+	 * @param performanceName
+	 * @return
+	 */
+	List<Performance> selectPerformanceByPerformanceName(String performanceName);
+	/**
+	 * 공연정보 날짜로 검색
+	 * @param performanceDate
+	 * @return
+	 */
+	List<Performance> selectPerformanceByPerformanceDate(Date performanceDate);
+	/**
+	 * 공연정보 내용으로 검색
+	 * @param performanceContent
+	 * @return
+	 */
+	List<Performance> selectPerformanceByPerformanceContent(String performanceContent);
 
 }
