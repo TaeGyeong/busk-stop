53516257863ab6a2aa3612fca35515e866bfd0eb
YoonKyuChoi <dbsrb05111@naver.com>
2017-11-29 10:14:00 +0900

    어노테이션

--- buskstop/src/main/java/com/buskstop/service/impl/AuthorityServiceImpl.java
+++ buskstop/src/main/java/com/buskstop/service/impl/AuthorityServiceImpl.java
@@ -3,11 +3,12 @@ package com.buskstop.service.impl;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.buskstop.dao.AuthorityDao;
 import com.buskstop.service.AuthorityService;
 import com.buskstop.vo.Authority;
-
+@Service
 public class AuthorityServiceImpl implements AuthorityService{
 
 	@Autowired
