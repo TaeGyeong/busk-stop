8998667b2fbd044702e19084adc703e6bf3f8a11
LeeYuRi <dldbfl7844@gmail.com>
2017-12-07 11:06:07 +0900

    ERD수정으로 인한 VO와 SQL 수정

--- buskstop/src/main/java/com/buskstop/vo/PremiumStageReservation.java
+++ buskstop/src/main/java/com/buskstop/vo/PremiumStageReservation.java
@@ -2,49 +2,83 @@ package com.buskstop.vo;
 
 public class PremiumStageReservation {
 
-		private int establishNo;
-		private String UserId;
-		private int stageState;
+		private int reservationNo; //옵션 번호
+		private int stageState; //예약상태 : 0 - 신청가능(거절), 1 - 신청중(수락대기), 2 - 대관완료(수락)
+		private String UserId; //예약신청자 ID
+		private int establishNo; //사업장번호
+		
 		
 		public PremiumStageReservation() {}
-		
-		public PremiumStageReservation(int establishNo, String userId, int stageState) {
-			this.establishNo = establishNo;
-			UserId = userId;
+
+
+		public PremiumStageReservation(int reservationNo, int stageState, String userId, int establishNo) {
+			super();
+			this.reservationNo = reservationNo;
 			this.stageState = stageState;
-		}
-		@Override
-		public String toString() {
-			return "PremiumStageReservation [establishNo=" + establishNo + ", UserId=" + UserId + ", stageState="
-					+ stageState + "]";
-		}
-		public int getEstablishNo() {
-			return establishNo;
-		}
-		public void setEstablishNo(int establishNo) {
+			UserId = userId;
 			this.establishNo = establishNo;
 		}
-		public String getUserId() {
-			return UserId;
+
+
+		public int getReservationNo() {
+			return reservationNo;
 		}
-		public void setUserId(String userId) {
-			UserId = userId;
+
+
+		public void setReservationNo(int reservationNo) {
+			this.reservationNo = reservationNo;
 		}
+
+
 		public int getStageState() {
 			return stageState;
 		}
+
+
 		public void setStageState(int stageState) {
 			this.stageState = stageState;
 		}
+
+
+		public String getUserId() {
+			return UserId;
+		}
+
+
+		public void setUserId(String userId) {
+			UserId = userId;
+		}
+
+
+		public int getEstablishNo() {
+			return establishNo;
+		}
+
+
+		public void setEstablishNo(int establishNo) {
+			this.establishNo = establishNo;
+		}
+
+
+		@Override
+		public String toString() {
+			return "PremiumStageReservation [reservationNo=" + reservationNo + ", stageState=" + stageState
+					+ ", UserId=" + UserId + ", establishNo=" + establishNo + "]";
+		}
+
+
 		@Override
 		public int hashCode() {
 			final int prime = 31;
 			int result = 1;
 			result = prime * result + ((UserId == null) ? 0 : UserId.hashCode());
 			result = prime * result + establishNo;
+			result = prime * result + reservationNo;
 			result = prime * result + stageState;
 			return result;
 		}
+
+
 		@Override
 		public boolean equals(Object obj) {
 			if (this == obj)
@@ -61,10 +95,11 @@ public class PremiumStageReservation {
 				return false;
 			if (establishNo != other.establishNo)
 				return false;
+			if (reservationNo != other.reservationNo)
+				return false;
 			if (stageState != other.stageState)
 				return false;
 			return true;
 		}
-		
 		
 }
--- buskstop/src/main/java/com/buskstop/vo/PremiumStageReservationTime.java
+++ buskstop/src/main/java/com/buskstop/vo/PremiumStageReservationTime.java
@@ -4,22 +4,17 @@ import java.util.Date;
 
 public class PremiumStageReservationTime {
 
-	private int timeCode;
-	private Date stageRentalDate;
-	private int establishNo;
+	private int timeCode; //0~24시를 1~24로 정함
+	private Date stageRentalDate; //시간 아닌 날짜만
+	private int reservationNo; //사업장 번호
 	
 	public PremiumStageReservationTime() {}
-	
-	public PremiumStageReservationTime(int timeCode, Date stageRentalDate, int establishNo) {
+
+	public PremiumStageReservationTime(int timeCode, Date stageRentalDate, int reservationNo) {
+		super();
 		this.timeCode = timeCode;
 		this.stageRentalDate = stageRentalDate;
-		this.establishNo = establishNo;
-	}
-
-	@Override
-	public String toString() {
-		return "PremiumStageReservationTime [timeCode=" + timeCode + ", stageRentalDate=" + stageRentalDate
-				+ ", establishNo=" + establishNo + "]";
+		this.reservationNo = reservationNo;
 	}
 
 	public int getTimeCode() {
@@ -38,19 +33,25 @@ public class PremiumStageReservationTime {
 		this.stageRentalDate = stageRentalDate;
 	}
 
-	public int getEstablishNo() {
-		return establishNo;
+	public int getReservationNo() {
+		return reservationNo;
 	}
 
-	public void setEstablishNo(int establishNo) {
-		this.establishNo = establishNo;
+	public void setReservationNo(int reservationNo) {
+		this.reservationNo = reservationNo;
+	}
+
+	@Override
+	public String toString() {
+		return "PremiumStageReservationTime [timeCode=" + timeCode + ", stageRentalDate=" + stageRentalDate
+				+ ", reservationNo=" + reservationNo + "]";
 	}
 
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + establishNo;
+		result = prime * result + reservationNo;
 		result = prime * result + ((stageRentalDate == null) ? 0 : stageRentalDate.hashCode());
 		result = prime * result + timeCode;
 		return result;
@@ -65,7 +66,7 @@ public class PremiumStageReservationTime {
 		if (getClass() != obj.getClass())
 			return false;
 		PremiumStageReservationTime other = (PremiumStageReservationTime) obj;
-		if (establishNo != other.establishNo)
+		if (reservationNo != other.reservationNo)
 			return false;
 		if (stageRentalDate == null) {
 			if (other.stageRentalDate != null)
@@ -76,8 +77,5 @@ public class PremiumStageReservationTime {
 			return false;
 		return true;
 	}
-	
-	
-	
 	
 }
